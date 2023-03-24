4d2d4315763bd380b7ff72068df8861f25414d6a
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2023-03-24 11:36:28 -0300

    add casos de teste do component Dialog

---
+++ src/components/Dialog/test/Dialog.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+describe("Dialog component test", () => {
+    it.todo("it should be visible when props isOpen is true");
+    it.todo("it should not be visible when props isOpen is false");
+    it.todo("it should render title");
+    it.todo("it should not render title");
+    it.todo("it should render children content");
+    it.todo("it should not render children content");
+    it.todo("it should call onClose method when click on icon");
+    it.todo("it should call onClose method when Esc is clicked");
+    it.todo("it should call onClose method when Overlay is clicked and closeOnOverlayClick is true");
+    it.todo("it should not call onClose method when Overlay is clicked and closeOnOverlayClick is false");
+});
